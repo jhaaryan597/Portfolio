ea265ae578d08750a5989aded9a07ca39ace27e8
Aryan Jha <aryanjha597@gmail.com>
2024-10-12 17:10:31 +0530

    new commit

---
+++ src/components/Projects.js
@@ -0,0 +1,78 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Brainwave",
+      description: "Explore the Possibilities of AI Chatting",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Gemini UI Clone",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>I have built Brainwave, an AI-powered chat application designed to optimize productivity by leveraging advanced natural language processing and smart automation. The app offers seamless integration and provides users with quick, accurate responses across various platforms.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>I have built Brainwave, an AI-powered chat application designed to optimize productivity by leveraging advanced natural language processing and smart automation. The app offers seamless integration and provides users with quick, accurate responses across various platforms.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Additionally, I developed a Gemini UI clone, meticulously replicating the design and functionality of the original platform. This project allowed me to refine my front-end development skills, focusing on creating a smooth user experience and design consistency.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
